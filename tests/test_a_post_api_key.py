a7e4311e14625ea33409f786c50e70372267e899
IrenCh88 <irenchechil@gmail.com>
2023-10-02 13:56:51 +0800

    Создание  tests folder

---
+++ tests/test_a_post_api_key.py
@@ -0,0 +1,15 @@
+from api import PetDemoProject
+from context import valid_username, valid_password
+
+
+pdp = PetDemoProject()
+
+# Получение токена
+def test_post_AuthToken_valid_admin(username=valid_username, password=valid_password):
+    status, result = pdp.post_api_AuthToken(username, password)
+    assert status == 200
+
+    assert "token" in result
+
+    token = result["token"]
+c
